1e638833ac704ed8301c323d947826da25e3a116
DBRI\yuz <yuzh@hhsc.ca>
2024-09-27 14:27:17 -0400

    1. initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FindNodes from './components/FindNodes.vue'
+</script>
+
+<template>
+  <div>
+    <FindNodes msg="Please input the content below:" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FindNodes.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <p>Multiline message is:</p>
+    <textarea v-model="message" placeholder="add multiple lines"></textarea>
+    <button style="color: brown;" @click="submitContent">Submit</button>
+    <9>Total</9>
+</template>
+
+<style scoped>
+ p{
+  font-size: 20px;
+  color:blueviolet;
+ }
+
+ textarea{
+  width: 800px;
+  height:400px;
+ }
+</style>
