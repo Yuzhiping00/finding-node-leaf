a40e7c4078f613b9aaf88bfb052a54b4a1fceb67
DBRI\yuz <yuzh@hhsc.ca>
2024-09-30 15:59:34 -0400

    This is a final test only

--- src/components/FindNodes.vue
+++ src/components/FindNodes.vue
@@ -94,6 +94,7 @@ watch(methodText, (newContent)=>{
   <div v-if="leadingSpacesErrorMsg" class="error-style">
     {{ leadingSpacesErrorMsg }}
   </div>
+  <p>This is a test for deploy to github pages</p>
 </template>
 
 <style scoped>
