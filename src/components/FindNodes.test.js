1f967dce0775d6d15ffa5fff519de68aeef773b3
DBRI\yuz <yuzh@hhsc.ca>
2024-10-08 12:43:08 -0400

    update unit test to pass it

--- src/components/FindNodes.test.js
+++ src/components/FindNodes.test.js
@@ -25,7 +25,7 @@ describe('FindNodes.vue', () =>{
         "  EMRAdmin.runAlert(HttpServletRequest, HttpServletResponse)  (com.hinext.emr.servlet)\n"+
         "       EMRAdmin.doPost(HttpServletRequest, HttpServletResponse)  (com.hinext.emr.servlet)\n"+
         "        EMRAdmin.doGet(HttpServletRequest, HttpServletResponse)  (com.hinext.emr.servlet)"
-        expect(wrapper.vm.calculateNodeLeaf()).toBe(3)
+        expect(wrapper.vm.calculateNodeLeaf()).toBe(2)
     })   
 
 })
